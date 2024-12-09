0f3c60c7653c6cf35bb304e2b60845ee08c65370
Varvara Brussueva <164172939+rinne13@users.noreply.github.com>
2024-12-09 14:50:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-perusteet-02-part2",
+  base: "react-perusteet-02-part2",
   plugins: [react()],  
 
   
